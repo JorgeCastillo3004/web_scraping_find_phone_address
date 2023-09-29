fbe22fa653eacc9076398253279fe07c5ccae3f2
jorge <fundest.jcastillo@gmail.com>
2023-09-28 22:42:10 -0400

    new update

--- test.py
+++ test.py
@@ -23,9 +23,25 @@ import linecache
 import sys
 from database import *
 
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(options=options)
+# options = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(options=options)
+# driver.get("https://www.fastpeoplesearch.com/")
 
-driver.get("https://www.fastpeoplesearch.com/")
+
+def launchNavigator():
+    global options, driver
+    options = webdriver.ChromeOptions()
+    optionsConfiguration()
+    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+    options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
+    driver = webdriver.Chrome(options=options)
+    # Changing the property of the navigator value for webdriver to undefined     
+
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  #Error
+    driver.get('https://www.fastpeoplesearch.com/')
+    # wait_search_box(max_try = 15)
+    # SelectSearch(by_name = True)
+
+launchNavigator()
 
 time.sleep(180)
