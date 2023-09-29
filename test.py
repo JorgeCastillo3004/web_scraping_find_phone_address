dcd119b6992ed2fcf79cfdb263cb805611394410
jorge <fundest.jcastillo@gmail.com>
2023-09-28 23:03:43 -0400

    Automated commit

--- test.py
+++ test.py
@@ -23,10 +23,6 @@ import linecache
 import sys
 from database import *
 
-# options = webdriver.ChromeOptions()
-# driver = webdriver.Chrome(options=options)
-# driver.get("https://www.fastpeoplesearch.com/")
-
 def optionsConfiguration():
     global options
     # Adding argument to disable the AutomationControlled flag 
@@ -47,20 +43,23 @@ def optionsConfiguration():
     # Define profile folder, profile number
     # options.add_argument(r"profile-directory=Profile 2")
         # launch chrome navigator
-        
-def launchNavigator():
-    global options, driver
-    options = webdriver.ChromeOptions()
-    optionsConfiguration()
-    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
-    options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
-    driver = webdriver.Chrome(options=options)
-    # Changing the property of the navigator value for webdriver to undefined     
 
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  #Error
-    driver.get('https://www.fastpeoplesearch.com/')
-    # wait_search_box(max_try = 15)
-    # SelectSearch(by_name = True)
+def launchNavigator():
+	global options, driver
+	# options = webdriver.ChromeOptions()
+	# # optionsConfiguration()
+	# options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+	# options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
+	# driver = webdriver.Chrome(options=options)
+	# # Changing the property of the navigator value for webdriver to undefined     
+
+	# driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  #Error
+	# driver.get('https://www.fastpeoplesearch.com/')
+	# wait_search_box(max_try = 15)
+	# SelectSearch(by_name = True)
+	options = webdriver.ChromeOptions()
+	driver = webdriver.Chrome(options=options)
+	driver.get("https://www.fastpeoplesearch.com/")
 
 launchNavigator()
 
